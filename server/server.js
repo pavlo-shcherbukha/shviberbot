b6d78f793bd5af63686da0b6af1b0b39c2d5c370
Pavlo Shcherbukha <pavlo.shcherbukha@pravex.ua>
2023-05-31 21:41:49 +0300

    s2

--- server/server.js
+++ server/server.js
@@ -94,10 +94,10 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
 });
 
 // Bind the bot middleware to the app instance
-//app.use(bot_expose_uri_path, bot.middleware());
+app.use(bot_expose_uri_path, bot.middleware());
 
 
-app.post('/api/webhook',  function(req, res) {
+app.post('/api2/webhook',  function(req, res) {
   label='http-post:api-webhook' 
   try {
     
@@ -170,12 +170,12 @@ if(!module.parent){
     applog.info( 'SERVER HAS STARTED',label);
     applog.info( `LISTENING  PORT= ${port} on HOST ${ ( typeof process.env.HOSTNAME === "undefined") ? 'localhost' :  process.env.HOSTNAME}`,label);
   
-    /*
+    
     bot.setWebhook(bot_expose_domain + bot_expose_uri_path)
     .catch(error => {
       logger.debug(`Error: The webhook ${bot_expose_domain + bot_expose_uri_path} cannot be set. ${error.message}`);
       process.exit(1);
-    });*/
+    });
     
     
 
